6797c42550f62e9eb9fcc7362c443b39cedc93bf
Nick Fox-Gieg <nick@fox-gieg.com>
2022-03-27 02:35:17 -0400

    shell

---
+++ setup.sh
@@ -0,0 +1,7 @@
+pip install -r requirements.txt
+
+wget https://fox-gieg.com/patches/github/n1ckfg/informative-drawings/models/checkpoints.zip
+unzip checkpoints.zip
+rm checkpoints.zip
+
+
---
+++ test.sh
@@ -0,0 +1,4 @@
+STYLE=anime_style
+#STYLE=opensketch_style
+
+python test.py --name $STYLE --dataroot examples/test
