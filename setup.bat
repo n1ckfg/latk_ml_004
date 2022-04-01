69deae0a57a7f003dbb2ce253b35b3b8a114f28f
Nick Fox-Gieg <nick@fox-gieg.com>
2022-04-01 13:23:33 -0400

    shell scripts

--- setup.bat
+++ setup.bat
@@ -1,6 +1,7 @@
 @echo off
 
-pip install -r requirements.txt
+pip install -r requirements-win.txt
+pip install --pre torch torchvision -f https://download.pytorch.org/whl/nightly/cu113/torch_nightly.html
 
 cd %~dp0
 powershell -Command "Invoke-WebRequest https://fox-gieg.com/patches/github/n1ckfg/informative-drawings/models/checkpoints.zip -OutFile checkpoints.zip"
