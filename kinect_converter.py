f7ba89f121d85b75eeeb13f7781440f6e9d39df9
Nick Fox-Gieg <nick@fox-gieg.com>
2022-04-01 10:03:54 -0400

    Update kinect_converter.py

--- kinect_converter.py
+++ kinect_converter.py
@@ -1,32 +1,33 @@
 import numpy as np
 
-# https://docs.opencv.org/4.x/dc/d3a/classcv_1_1viz_1_1Camera.html#a753809aff611cdcc5a8a377126156b05
 # Generic
-cx = 320
-cy = 240
-focalx = 525
-focaly = 525
+# https://en.wikipedia.org/wiki/Camera_resectioning
+# https://docs.opencv.org/4.x/dc/d3a/classcv_1_1viz_1_1Camera.html#a753809aff611cdcc5a8a377126156b05
+cx = 320 # principal point x
+cy = 240 # principal point y
+fx = 525 # focal length x
+fy = 525 # focal length y
 
 '''
 # Azure Kinect (4K mode)
 # https://forum.open3d.org/t/azure-kinect-intrinsic-structure/121
 cx = 2044.2911376953125
 cy = 1565.8837890625
-focalx = 1959.37890625
-focaly = 1958.880126953125
+fx = 1959.37890625
+fy = 1958.880126953125
 
 # Kinect 2
 # https://medium.com/yodayoda/from-depth-map-to-point-cloud-7473721d3f
 cx = 254.878
 cy = 205.395
-focalx = 365.456
-focaly = 365.456
+fx = 365.456
+fy = 365.456
 '''
 
 def uvd_to_xyz(u, v, d, scale=1):
     d *= scale
-    x_over_z = (cx - u) / focalx
-    y_over_z = (cy - v) / focaly
+    x_over_z = (cx - u) / fx
+    y_over_z = (cy - v) / fy
 
     z = d / np.sqrt(1. + x_over_z**2 + y_over_z**2)
     x = x_over_z * z
