38c16b2e5044be9ed1f6742b531d9358f84be421
Nick Fox-Gieg <nick@fox-gieg.com>
2022-04-04 10:41:03 -0400

    fix x flip

--- skeletonizer.py
+++ skeletonizer.py
@@ -103,7 +103,7 @@ for i in range(0, len(lineFilesList)):
             rgbPixel2 = (rgbPixel[2] / 255, rgbPixel[1] / 255, rgbPixel[0] / 255, 1)
 
             co = kc.uvd_to_xyz(u=point[0], v=point[1], d=abs(255 - depthPixel[0]), scale=0.1)
-            co2 = (co[0], co[2], co[1])
+            co2 = (-co[0], co[2], co[1])
             lPoint = latk.LatkPoint(co2)
             lPoint.vertex_color = rgbPixel2
             lPoints.append(lPoint)
