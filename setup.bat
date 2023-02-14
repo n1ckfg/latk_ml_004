7718c8f09b4325d4f235551decacaab087c26648
Nick Fox-Gieg <nick@fox-gieg.com>
2023-02-14 15:53:06 -0500

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,7 +1,8 @@
 @echo off
 
 pip install -r requirements-win.txt
-pip install --pre torch torchvision -f https://download.pytorch.org/whl/nightly/cu113/torch_nightly.html
+rem pip install --pre torch torchvision -f https://download.pytorch.org/whl/nightly/cu113/torch_nightly.html
+pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu116
 
 cd %~dp0
 powershell -Command "Invoke-WebRequest https://fox-gieg.com/patches/github/n1ckfg/informative-drawings/models/checkpoints.zip -OutFile checkpoints.zip"
