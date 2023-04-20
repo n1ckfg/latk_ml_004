fc9401d7add80bdcd7fa30a30183dfef115d08d4
Nick Fox-Gieg <nick@fox-gieg.com>
2023-04-20 14:55:51 -0400

    shell os x compile swig

--- skeleton-tracing/swig/compile.bat
+++ skeleton-tracing/swig/compile.bat
@@ -15,13 +15,6 @@ rem g++ -shared $(python3-config --cflags --ldflags) *.o -o _trace_skeleton.so
 gcc -fPIC -O3 -c trace_skeleton.c trace_skeleton_wrap.c -I%HOMEDRIVE%%HOMEPATH%\AppData\Local\Programs\Python\Python310\include
 g++ -shared *.o -o _trace_skeleton.pyd -L%HOMEDRIVE%%HOMEPATH%\AppData\Local\Programs\Python\Python310\libs -lpython310
 
-rem OS X
-rem PYTHON_PATH=/usr/local/Cellar/python/3.7.6_1/Frameworks/Python.framework/Versions/3.7
-rem PYTHON_INCLUDE=$PYTHON_PATH/include/python3.7m
-rem PYTHON_LIB=$PYTHON_PATH/lib/libpython3.7m.dylib
-rem gcc -O3 -c trace_skeleton.c trace_skeleton_wrap.c -I$PYTHON_INCLUDE
-rem gcc $(python3-config --ldflags) -dynamiclib *.o -o _trace_skeleton.so -I$PYTHON_LIB -undefined dynamic_lookup
-
 rem quick tests
 rem python3 -i -c "import trace_skeleton; trace_skeleton.trace('\0\0\0\1\1\1\0\0\0',3,3); print(trace_skeleton.len_polyline());"
 rem python3 -i -c "import trace_skeleton; print(trace_skeleton.from_list([0,0,0,1,1,1,0,0,0],3,3))"
--- skeleton-tracing/swig/compile.command
+++ skeleton-tracing/swig/compile.command
@@ -1,11 +1,13 @@
 swig -python trace_skeleton.i
 
 # OS X
-PYTHON_VERSION=3.9
-PYTHON_VERSION_FULL=3.9.12
-PYTHON_PATH=/usr/local/Cellar/python@$PYTHON_VERSION/$PYTHON_VERSION_FULL/Frameworks/Python.framework/Versions/$PYTHON_VERSION
+PYTHON_VERSION=3.10
+PYTHON_VERSION_FULL=3.10.6
+#PYTHON_PATH=/usr/local/Cellar/python@$PYTHON_VERSION/$PYTHON_VERSION_FULL/Frameworks/Python.framework/Versions/$PYTHON_VERSION
+PYTHON_PATH=~/.pyenv/versions/$PYTHON_VERSION_FULL
 PYTHON_INCLUDE=$PYTHON_PATH/include/python$PYTHON_VERSION
-PYTHON_LIB=$PYTHON_PATH/lib/libpython$PYTHON_VERSION.dylib
+PYTHON_LIB=$PYTHON_PATH/lib/libpython$PYTHON_VERSION.a
+
 gcc -O3 -c trace_skeleton.c trace_skeleton_wrap.c -I$PYTHON_INCLUDE
 gcc $(python3-config --ldflags) -dynamiclib *.o -o _trace_skeleton.so -I$PYTHON_LIB -undefined dynamic_lookup
 
